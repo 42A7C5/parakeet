d5ffded6427298e7680b613ebf033cd3dbd162a3
Samuel Sharp <samuelsharp4@gmail.com>
2022-12-01 17:51:20 +0000

    feat: :sparkles: Added game listings to homepage
    Also refined navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,11 @@ import styles from '../styles/Navbar.module.css'
 export default function App({ Component, pageProps }: AppProps) {
   return <>
     <header>
-      <span className={styles.navTitle}>Parakeet</span>
+      <span className={styles.navTitle}><Link href={'/'} className={styles.navTitleLink}>Parakeet</Link></span>
       <span className={styles.navLinks}>
-        <Link href={'/store'} className={styles.navLink}>🏬</Link>
-        <Link href={'/account'} className={styles.navLink}>👦</Link>
-        <Link href={'/about'} className={styles.navLink}>ℹ️</Link>
+        <Link href={'/store'} className={styles.navLink}><span className="material-symbols-outlined">storefront</span></Link>
+        <Link href={'/account'} className={styles.navLink}><span className="material-symbols-outlined">person</span></Link>
+        <Link href={'/about'} className={styles.navLink}><span className="material-symbols-outlined">info</span></Link>
       </span>
     </header>
     <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,30 @@
+import styles from '../styles/Home.module.css'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import { Carousel } from 'react-responsive-carousel'
+
 export default function Home() {
   return (
-    <></>
+    <>
+      <div className={styles.gameList}>
+        <div className={styles.game} style={{ background: 'url(https://dragondungeon.io/assets/img/game/bt-bg.png) center center no-repeat', boxShadow: '0 0 20px red' }}>
+          <img src="https://dragondungeon.io/assets/img/skins/basic.png" alt="" />
+        </div>
+        <div className={styles.game} style={{ background: 'url(https://dragondungeon.io/assets/img/game/bt-bg.png) center center no-repeat', boxShadow: '0 0 20px red' }}>
+          <img src="https://dragondungeon.io/assets/img/skins/basic.png" alt="" />
+        </div>
+        <div className={styles.game} style={{ background: 'url(https://dragondungeon.io/assets/img/game/bt-bg.png) center center no-repeat', boxShadow: '0 0 20px red' }}>
+          <img src="https://dragondungeon.io/assets/img/skins/basic.png" alt="" />
+        </div>
+        <div className={styles.game} style={{ background: 'url(https://dragondungeon.io/assets/img/game/bt-bg.png) center center no-repeat', boxShadow: '0 0 20px red', border: '0px solid red' }}>
+          <img src="https://dragondungeon.io/assets/img/skins/basic.png" alt="" />
+        </div>
+        <div className={styles.game} style={{ background: 'url(https://dragondungeon.io/assets/img/game/bt-bg.png) center center no-repeat', boxShadow: '0 0 20px red', border: '0px solid red' }}>
+          <img src="https://dragondungeon.io/assets/img/skins/basic.png" alt="" />
+        </div>
+        <div className={styles.game} style={{ background: 'url(https://dragondungeon.io/assets/img/game/bt-bg.png) center center no-repeat', boxShadow: '0 0 20px red', border: '0px solid red' }}>
+          <img src="https://dragondungeon.io/assets/img/skins/basic.png" alt="" />
+        </div>
+      </div>
+    </>
   )
 }
