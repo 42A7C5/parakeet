f0716483eb5f60621c4e78ab886e9df0e5e8ff55
Samuel Sharp <samuelsharp4@gmail.com>
2022-12-02 21:15:10 +0000

    Add game feature section

---
+++ pages/about.tsx
@@ -0,0 +1,6 @@
+export default function About() {
+    return <div style={{ padding: '40px', textAlign: 'center' }}>
+        <h1>Premium gaming for the price of free.</h1>
+        <h2>Parakeet is the best place to play games anywhere, anytime, for free.</h2>
+    </div>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,16 @@ export default function Home() {
   return (
     <>
       <div className={styles.gameList}>
-        <div className={styles.game} style={{ background: 'url(https://dragondungeon.io/assets/img/game/bt-bg.png) center center no-repeat', boxShadow: '0 0 20px red' }}>
+        <div className={styles.gameotw} style={{ background: 'url(https://dragondungeon.io/assets/img/game/bt-bg.png) center center no-repeat', boxShadow: '0 0 20px red', border: '0px solid red' }}>
+          <div>
+            <img src="https://dragondungeon.io/assets/img/skins/basic.png" style={{ height: '120px' }} />
+            <p><b>Featured Game</b> | Play the exclusive Holiday Mayhem event through Jan 1.</p>
+          </div>
+        </div>
+        <div className={styles.game} style={{ background: 'url(https://dragondungeon.io/assets/img/game/bt-bg.png) center center no-repeat', boxShadow: '0 0 20px red', border: '0px solid red' }}>
           <img src="https://dragondungeon.io/assets/img/skins/basic.png" alt="" />
         </div>
-        <div className={styles.game} style={{ background: 'url(https://dragondungeon.io/assets/img/game/bt-bg.png) center center no-repeat', boxShadow: '0 0 20px red' }}>
-          <img src="https://dragondungeon.io/assets/img/skins/basic.png" alt="" />
-        </div>
-        <div className={styles.game} style={{ background: 'url(https://dragondungeon.io/assets/img/game/bt-bg.png) center center no-repeat', boxShadow: '0 0 20px red' }}>
+        <div className={styles.game} style={{ background: 'url(https://dragondungeon.io/assets/img/game/bt-bg.png) center center no-repeat', boxShadow: '0 0 20px red', border: '0px solid red' }}>
           <img src="https://dragondungeon.io/assets/img/skins/basic.png" alt="" />
         </div>
         <div className={styles.game} style={{ background: 'url(https://dragondungeon.io/assets/img/game/bt-bg.png) center center no-repeat', boxShadow: '0 0 20px red', border: '0px solid red' }}>
