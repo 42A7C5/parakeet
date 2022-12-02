f51867aa91821812907dd47e73b1bddd78b23cad
Samuel Sharp <samuelsharp4@gmail.com>
2022-12-02 21:31:04 +0000

    Add carousel

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,26 @@ export default function Home() {
   return (
     <>
       <div className={styles.gameList}>
-        <div className={styles.gameotw} style={{ background: 'url(https://dragondungeon.io/assets/img/game/bt-bg.png) center center no-repeat', boxShadow: '0 0 20px red', border: '0px solid red' }}>
-          <div>
-            <img src="https://dragondungeon.io/assets/img/skins/basic.png" style={{ height: '120px' }} />
-            <p><b>Featured Game</b> | Play the exclusive Holiday Mayhem event through Jan 1.</p>
+        <Carousel className={styles.carousel} showArrows={false} showThumbs={false} showStatus={false} autoPlay={true}>
+          <div className={styles.gameotw} style={{ background: 'url(https://dragondungeon.io/assets/img/game/bt-bg.png) center center no-repeat', boxShadow: '0 0 10px red', border: '0px solid red' }}>
+            <div>
+              <img src="https://dragondungeon.io/assets/img/skins/basic.png" style={{ height: '120px', width: 'auto' }} />
+              <p><b>Featured Game</b> | Play the exclusive Holiday Mayhem event through Jan 1.</p>
+            </div>
           </div>
-        </div>
+          <div className={styles.gameotw} style={{ background: 'url(https://dragondungeon.io/assets/img/game/bt-bg.png) center center no-repeat', boxShadow: '0 0 10px red', border: '0px solid red' }}>
+            <div>
+              <img src="https://dragondungeon.io/assets/img/skins/basic.png" style={{ height: '120px', width: 'auto' }} />
+              <p><b>Featured Game</b> | Play the exclusive Holiday Mayhem event through Jan 1.</p>
+            </div>
+          </div>
+          <div className={styles.gameotw} style={{ background: 'url(https://dragondungeon.io/assets/img/game/bt-bg.png) center center no-repeat', boxShadow: '0 0 10px red', border: '0px solid red' }}>
+            <div>
+              <img src="https://dragondungeon.io/assets/img/skins/basic.png" style={{ height: '120px', width: 'auto' }} />
+              <p><b>Featured Game</b> | Play the exclusive Holiday Mayhem event through Jan 1.</p>
+            </div>
+          </div>
+        </Carousel>
         <div className={styles.game} style={{ background: 'url(https://dragondungeon.io/assets/img/game/bt-bg.png) center center no-repeat', boxShadow: '0 0 20px red', border: '0px solid red' }}>
           <img src="https://dragondungeon.io/assets/img/skins/basic.png" alt="" />
         </div>
