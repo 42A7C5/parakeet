097aa88a47d7ede48ebdf7909f4ff4d5a0cce329
Samuel Sharp <samuelsharp4@gmail.com>
2022-12-09 08:34:57 -0800

    fix: :ambulance: Fix iterator key issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,8 +23,8 @@ export default function Home(props: any) {
       <div className={styles.gameList}>
         <Carousel className={styles.carousel} showArrows={false} showThumbs={false} showStatus={false} autoPlay={true}>
           {props.picks.map((game: any) => (
-            <Link href={`/play/${game.id}`}>
-              <div key={game.id} className={styles.gameotw} style={{ background: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${game.art.background}) center center no-repeat`, boxShadow: `0 0 20px ${game.color}`, border: `0px solid ${game.color}` }}>
+            <Link key={game.id} href={`/play/${game.id}`}>
+              <div className={styles.gameotw} style={{ background: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${game.art.background}) center center no-repeat`, boxShadow: `0 0 20px ${game.color}`, border: `0px solid ${game.color}` }}>
                 <div>
                   <img src={game.art.logo} style={{ height: '120px', width: 'auto' }} alt={game.name} />
                   <p style={{ fontSize: '18pt', fontWeight: 'bold', color: 'white' }}>{game.reason}</p>
@@ -34,8 +34,8 @@ export default function Home(props: any) {
           ))}
         </Carousel>
         {props.games.map((game: any) => (
-          <Link href={`/play/${game.id}`} style={{ flex: '300px' }}>
-            <div key={game.id} className={styles.game} style={{ background: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${game.art.background}) center center no-repeat`, boxShadow: `0 0 30px ${game.color}`, border: `0px solid ${game.color}` }}>
+          <Link key={game.id}  href={`/play/${game.id}`} style={{ flex: '300px' }}>
+            <div className={styles.game} style={{ background: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${game.art.background}) center center no-repeat`, boxShadow: `0 0 30px ${game.color}`, border: `0px solid ${game.color}` }}>
               <img src={game.art.logo} width={350} alt={game.name} />
             </div>
           </Link>
