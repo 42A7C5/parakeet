15d9d57ac842aace411ebd5fd5ea1ae28bb5c545
Samuel Sharp <samuelsharp4@gmail.com>
2023-01-02 18:58:06 -0800

    Update transition effect to fix FOUC

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -52,14 +52,12 @@ export default function App({ Component, pageProps }: AppProps) {
           variants={{
             out: {
               opacity: 0,
-              y: 40,
               transition: {
                 duration: 0.4,
               },
             },
             in: {
               opacity: 1,
-              y: 0,
               transition: {
                 duration: 0.4,
               },
